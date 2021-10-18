9593e2e40afd565382fa03facce1920f6f94b7b3
Sha Mwe La <shamwela2002@gmail.com>
2021-10-18 12:10:38 +0630

    Improve implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
 import Head from 'next/head'
 
-export default function Home() {
+const Home = () => {
   const [pixel, setPixel] = useState(10)
   const [rem, setRem] = useState(0.625)
 
-  const handlePixel = (pixel: number) => {
-    setPixel(pixel)
-    setRem(pixel / 16)
+  const handlePixel = (pixel: string) => {
+    setPixel(Number(pixel))
+    setRem(Number(pixel) / 16)
   }
 
-  const handleRem = (rem: number) => {
-    setRem(rem)
-    setPixel(rem * 16)
+  const handleRem = (rem: string) => {
+    setRem(Number(rem))
+    setPixel(Number(rem) * 16)
   }
 
   return (
@@ -25,17 +25,17 @@ export default function Home() {
         />
       </Head>
 
-      <main className='max-w-5xl min-h-screen m-auto p-4 flex flex-col gap-y-4'>
+      <main className='max-w-3xl min-h-screen m-auto p-4 flex flex-col gap-y-4'>
         <h1>Pixel-rem Two-Way Converter</h1>
 
         <section className='flex flex-col gap-y-1'>
           <label htmlFor='pixel'>Pixel</label>
           <input
             value={pixel}
-            onChange={(event) => handlePixel(+event.currentTarget.value)}
+            onChange={(event) => handlePixel(event.currentTarget.value)}
             id='pixel'
             type='number'
-            min='1'
+            min='0'
           />
         </section>
 
@@ -43,10 +43,10 @@ export default function Home() {
           <label htmlFor='rem'>rem</label>
           <input
             value={rem}
-            onChange={(event) => handleRem(+event.currentTarget.value)}
+            onChange={(event) => handleRem(event.currentTarget.value)}
             id='rem'
             type='number'
-            min='1'
+            min='0'
           />
         </section>
 
@@ -55,3 +55,5 @@ export default function Home() {
     </>
   )
 }
+
+export default Home
