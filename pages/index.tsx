9f93d6fcaad454dcb1ebfdb1f2a3b67d1b7c4b59
Sha Mwe La <shamwela2002@gmail.com>
2021-10-24 16:11:34 +0630

    Format Tailwind CSS classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,8 +25,8 @@ const Home = () => {
         />
       </Head>
 
-      <main className='min-h-screen flex'>
-        <div className='w-full max-w-3xl m-auto p-4 flex flex-col gap-y-4'>
+      <main className='flex min-h-screen'>
+        <div className='flex flex-col w-full max-w-3xl p-4 m-auto gap-y-4'>
           <section className='flex flex-col gap-y-1'>
             <label htmlFor='pixel' className='ml-5'>
               Pixel
